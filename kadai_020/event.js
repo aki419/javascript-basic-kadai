25ec76686b46bf75b1727fcfe0ed128097813c07
aki419 <akinagano9241@gmail.com>
2025-01-24 01:23:12 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,11 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const clickBtn = document.getElementById('btn');
+
+
+// HTML要素がクリックされたときにイベント処理を実行する
+clickBtn.addEventListener('click', () => {
+  // textというidを持つHTML要素を取得し、定数に代入する
+  const changeText = document.getElementById('text');
+
+  changeText.textContent = 'ボタンをクリックしました';
+});
